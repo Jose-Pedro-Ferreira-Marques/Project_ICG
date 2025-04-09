52b9248062e8c981d8833ab916ffe2a6b8a756cd
Jose-Pedro-Ferreira-Marques <jpfmarques@ua.pt>
2025-04-09 15:00:58 +0100

    3

--- ICG/controls.js
+++
@@ -1,6 +0,0 @@
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
-
-export function setupControls(camera, playerBody) {
-    const controls = new PointerLockControls(camera, document.body);
-    document.addEventListener('click', () => controls.lock());
-}
